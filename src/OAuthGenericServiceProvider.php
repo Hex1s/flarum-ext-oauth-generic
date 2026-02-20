15841adec92cd13d13373b294584659687ab9d26
Hex1s <123328893+Hex1s@users.noreply.github.com>
2026-02-20 13:38:46 +0300

    fix

--- src/OAuthGenericServiceProvider.php
+++ src/OAuthGenericServiceProvider.php
@@ -7,10 +7,17 @@ use Illuminate\Contracts\Container\Container;
 
 class OAuthGenericServiceProvider
 {
-    public function register(Container $container): void
+    protected Container $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function register(): void
     {
         // Use our controller so OAuth runs in same tab (display=page), fixing existing-user re-login.
-        $container->bind(
+        $this->container->bind(
             AuthController::class,
             Controllers\GenericAuthController::class
         );
